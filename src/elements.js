645303fae4d214079d7c2e15f290823290c4300d
kirrrusha <kirilllebedenko@gmail.com>
2021-08-13 16:32:52 +0300

    add elements

--- src/elements.js
+++ src/elements.js
@@ -6,6 +6,9 @@ height: 62px;
 font-size:20px;
 text-align: center;
 color: white;
-background-color: ${(props) => (!!props.type && props.type === 'secondary') ? 'transparent' : '#FF5A05'}
+background-color: ${(props) => {
+    console.log(props);
+    return !!props.type && props.type === 'secondary' ? 'gray' : '#FF5A05'
+}};
 border: none;
 `;
